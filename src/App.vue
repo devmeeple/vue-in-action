c119564c31090b60bb473b0583e2f96f8699a174
미플 <devmeeple@gmail.com>
2024-08-05 23:25:22 +0900

    feat: Props 자식 컴포넌트에 전달하기

--- src/App.vue
+++ src/App.vue
@@ -1,171 +1,28 @@
 <template>
-  <div id="app">
-    <h1>To-Do List</h1>
-    <to-do-form @todo-added="addTodo"></to-do-form>
-    <ul aria-labelledby="list-summary" class="stack-large">
-      <li v-for="item in TodoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
-      </li>
-    </ul>
-  </div>
+  <div></div>
+  <ChildComponent :sendProps1="title" :sendProps2="year" :sendProps3="post" />
 </template>
 
 <script setup lang="ts">
-import ToDoItem from '@/components/ToDoItem.vue';
-import { nanoid } from 'nanoid';
-import ToDoForm from '@/components/ToDoForm.vue';
-import { ref } from 'vue';
+import ChildComponent from '@/components/ChildComponent.vue';
+import { reactive, ref } from 'vue';
 
-const TodoItems = ref([
-  { id: 'todo-' + nanoid(), label: 'Learn Vue', done: false },
-  {
-    id: 'todo-' + nanoid(),
-    label: 'Create a Vue project with the CLI',
-    done: true,
-  },
-  { id: 'todo-' + nanoid(), label: 'Have fun', done: true },
-  {
-    id: 'todo-' + nanoid(),
-    label: 'Create a to-do list',
-    done: false,
-  },
-]);
+export interface Post {
+  id: number;
+  title: string;
+  description: string;
+}
 
-const addTodo = (toDoLabel: string) => {
-  TodoItems.value.push({ id: 'todo-' + nanoid(), label: toDoLabel, done: false });
-};
+// props 전달할 데이터 정의
+const title = ref<string>('부모 컴포넌트에서 선언된 데이터');
+const year = ref<number>(2024);
+
+// 객체는 reactive 권장
+const post = reactive<Post>({
+  id: 1,
+  title: '최강야구',
+  description: '롯데 상대로 동점홈런을 날리는 이대호',
+});
 </script>
 
-<style>
-/* Global styles */
-.btn {
-  padding: 0.8rem 1rem 0.7rem;
-  border: 0.2rem solid #4d4d4d;
-  cursor: pointer;
-  text-transform: capitalize;
-}
-
-.btn__danger {
-  color: #fff;
-  background-color: #ca3c3c;
-  border-color: #bd2130;
-}
-
-.btn__filter {
-  border-color: lightgrey;
-}
-
-.btn__danger:focus {
-  outline-color: #c82333;
-}
-
-.btn__primary {
-  color: #fff;
-  background-color: #000;
-}
-
-.btn-group {
-  display: flex;
-  justify-content: space-between;
-}
-
-.btn-group > * {
-  flex: 1 1 auto;
-}
-
-.btn-group > * + * {
-  margin-left: 0.8rem;
-}
-
-.label-wrapper {
-  margin: 0;
-  flex: 0 0 100%;
-  text-align: center;
-}
-
-[class*='__lg'] {
-  display: inline-block;
-  width: 100%;
-  font-size: 1.9rem;
-}
-
-[class*='__lg']:not(:last-child) {
-  margin-bottom: 1rem;
-}
-
-@media screen and (min-width: 620px) {
-  [class*='__lg'] {
-    font-size: 2.4rem;
-  }
-}
-
-.visually-hidden {
-  position: absolute;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px 1px 1px 1px);
-  clip: rect(1px, 1px, 1px, 1px);
-  clip-path: rect(1px, 1px, 1px, 1px);
-  white-space: nowrap;
-}
-
-[class*='stack'] > * {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-
-.stack-small > * + * {
-  margin-top: 1.25rem;
-}
-
-.stack-large > * + * {
-  margin-top: 2.5rem;
-}
-
-@media screen and (min-width: 550px) {
-  .stack-small > * + * {
-    margin-top: 1.4rem;
-  }
-
-  .stack-large > * + * {
-    margin-top: 2.8rem;
-  }
-}
-
-/* End global styles */
-#app {
-  background: #fff;
-  margin: 2rem 0 4rem 0;
-  padding: 1rem;
-  padding-top: 0;
-  position: relative;
-  box-shadow:
-    0 2px 4px 0 rgb(0 0 0 / 20%),
-    0 2.5rem 5rem 0 rgb(0 0 0 / 10%);
-}
-
-@media screen and (min-width: 550px) {
-  #app {
-    padding: 4rem;
-  }
-}
-
-#app > * {
-  max-width: 50rem;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-#app > form {
-  max-width: 100%;
-}
-
-#app h1 {
-  display: block;
-  min-width: 100%;
-  width: 100%;
-  text-align: center;
-  margin: 0 0 1rem;
-}
-</style>
+<style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ sendProps1 }}</div>
+  <div>{{ sendProps2 }}</div>
+  <ul>
+    <li>{{ sendProps3.id }}</li>
+    <li>{{ sendProps3.title }}</li>
+    <li>{{ sendProps3.description }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { Post } from '@/App.vue';
+import { toRefs } from 'vue';
+
+// 1. Composition API
+// 1-1. Props 받은 데이터 타입을 지정한다.
+
+interface Props {
+  sendProps1: String;
+  sendProps2: Number;
+  sendProps3: Post;
+}
+
+// defineProps 내장함수 사용
+// toRefs: 전달 받은 데이터를 템플릿에서 호출해서 사용한다.
+const props = defineProps<Props>();
+const { sendProps1, sendProps2, sendProps3 } = toRefs(props);
+</script>
+
+<style scoped></style>
