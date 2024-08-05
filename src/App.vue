123c1c28a6181006d8417a3e5a8da63f9c5c2809
미플 <devmeeple@gmail.com>
2024-08-05 22:42:46 +0900

    feat(mdn): MDN Todo list

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,171 @@
 <template>
-  <button @click="changeMessage">{{ message }}</button>
-  <p>{{ watchMessage }}</p>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addTodo"></to-do-form>
+    <ul aria-labelledby="list-summary" class="stack-large">
+      <li v-for="item in TodoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import ToDoItem from '@/components/ToDoItem.vue';
+import { nanoid } from 'nanoid';
+import ToDoForm from '@/components/ToDoForm.vue';
+import { ref } from 'vue';
 
-const message = ref('[Watch 오리지널 문구] 놀러오세요 동물의 숲');
-const watchMessage = ref('');
+const TodoItems = ref([
+  { id: 'todo-' + nanoid(), label: 'Learn Vue', done: false },
+  {
+    id: 'todo-' + nanoid(),
+    label: 'Create a Vue project with the CLI',
+    done: true,
+  },
+  { id: 'todo-' + nanoid(), label: 'Have fun', done: true },
+  {
+    id: 'todo-' + nanoid(),
+    label: 'Create a to-do list',
+    done: false,
+  },
+]);
 
-const changeMessage = () => {
-  console.log('함수 호출');
-  message.value = '변경된 메세지';
+const addTodo = (toDoLabel: string) => {
+  TodoItems.value.push({ id: 'todo-' + nanoid(), label: toDoLabel, done: false });
 };
-
-watch(message, () => {
-  watchMessage.value = 'Watch 동작';
-});
 </script>
 
-<style scoped></style>
+<style>
+/* Global styles */
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
+}
+
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+
+.btn__filter {
+  border-color: lightgrey;
+}
+
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn-group > * {
+  flex: 1 1 auto;
+}
+
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
+  text-align: center;
+}
+
+[class*='__lg'] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+
+[class*='__lg']:not(:last-child) {
+  margin-bottom: 1rem;
+}
+
+@media screen and (min-width: 620px) {
+  [class*='__lg'] {
+    font-size: 2.4rem;
+  }
+}
+
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+
+[class*='stack'] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow:
+    0 2px 4px 0 rgb(0 0 0 / 20%),
+    0 2.5rem 5rem 0 rgb(0 0 0 / 10%);
+}
+
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#app > form {
+  max-width: 100%;
+}
+
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0 0 1rem;
+}
+</style>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg"> What needs to be done? </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// 이벤트 정의
+const emit = defineEmits(['todo-added']);
+
+// label 변수 정의
+const label = ref('');
+
+// onSubmit 함수 정의
+const onSubmit = () => {
+  if (label.value === '') {
+    return '';
+  }
+
+  emit('todo-added', label.value);
+  label.value = '';
+};
+</script>
+<style scoped></style>
